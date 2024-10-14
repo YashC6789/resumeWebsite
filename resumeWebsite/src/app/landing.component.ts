bbe31402a0ed7a667cdb48cb46c7dee9a14e44c8
Yash Chauhan <yashchauhan132017@gmail.com>
2024-10-14 18:52:12 -0400

    starting

---
+++ resumeWebsite/src/app/landing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-landing',
+    styles:``,
+    standalone: true,
+    templateUrl: './landing.component.html'
+})
+export class LandingComponent {
+    
+}
