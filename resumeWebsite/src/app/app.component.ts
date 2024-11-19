df6bfd88e1072c57027342987c6883007f4dcfac
Yash Chauhan <yashchauhan132017@gmail.com>
2024-11-19 17:21:45 -0500

    basic

--- resumeWebsite/src/app/app.component.ts
+++ resumeWebsite/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LandingComponent } from './landing.component';
+import { LandingComponent } from './Landing/landing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, LandingComponent],
   template: `
-      <app-landing/>
+    <nav class="navbar">
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/projects">Projects</a></li>
+        <!-- <li><a href="/experiences">Experience</a></li>
+        <li><a href="/interests">Interests</a></li>
+        <li><a href="/contact">Contact</a></li> -->
+      </ul>
+    </nav>
+    <router-outlet/>
   `,
   styleUrl: './app.component.scss'
 })
--- resumeWebsite/src/app/app.routes.ts
+++ resumeWebsite/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './Landing/landing.component';
+import { ProjectsComponent } from './Projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'App Home Page',
+        component: LandingComponent,
+    },
+    {
+        path: 'projects',
+        title: 'App Project Page',
+        component: ProjectsComponent,
+    },
+];
